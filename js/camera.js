579c2c84e76b7be8b4e32532024a21efe84f3f79
stoffer6740 <cbj@c-bjerregaard.dk>
2014-04-03 09:34:38 +0200

    Updated to 3.3.0

--- js/camera.js
+++ js/camera.js
@@ -1,11 +1,11 @@
-    var pictureSource;   // picture source
-    var destinationType; // sets the format of returned value 
+var pictureSource;   // picture source
+    var destinationType; // sets the format of returned value
 
-    // Wait for Cordova to connect with the device
+    // Wait for device API libraries to load
     //
     document.addEventListener("deviceready",onDeviceReady,false);
 
-    // Cordova is ready to be used!
+    // device APIs are available
     //
     function onDeviceReady() {
         pictureSource=navigator.camera.PictureSourceType;
@@ -15,7 +15,7 @@
     // Called when a photo is successfully retrieved
     //
     function onPhotoDataSuccess(imageData) {
-      // Uncomment to view the base64 encoded image data
+      // Uncomment to view the base64-encoded image data
       // console.log(imageData);
 
       // Get image handle
@@ -27,7 +27,7 @@
       smallImage.style.display = 'block';
 
       // Show the captured photo
-      // The inline CSS rules are used to resize the image
+      // The in-line CSS rules are used to resize the image
       //
       smallImage.src = "data:image/jpeg;base64," + imageData;
     }
@@ -35,7 +35,7 @@
     // Called when a photo is successfully retrieved
     //
     function onPhotoURISuccess(imageURI) {
-      // Uncomment to view the image file URI 
+      // Uncomment to view the image file URI
       // console.log(imageURI);
 
       // Get image handle
@@ -47,7 +47,7 @@
       largeImage.style.display = 'block';
 
       // Show the captured photo
-      // The inline CSS rules are used to resize the image
+      // The in-line CSS rules are used to resize the image
       //
       largeImage.src = imageURI;
     }
@@ -63,7 +63,7 @@
     // A button will call this function
     //
     function capturePhotoEdit() {
-      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string  
+      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
       navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
         destinationType: destinationType.DATA_URL });
     }
@@ -72,13 +72,13 @@
     //
     function getPhoto(source) {
       // Retrieve image file location from specified source
-      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
         destinationType: destinationType.FILE_URI,
         sourceType: source });
     }
 
     // Called if something bad happens.
-    // 
+    //
     function onFail(message) {
       alert('Failed because: ' + message);
     }
